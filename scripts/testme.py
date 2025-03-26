61d810534c1c3d47963d5ac3d0b6adab3e6c96c5
franking66 <messagefrank@outlook.com>
2025-03-25 21:41:56 -0400

    add files to the repo

---
+++ scripts/testme.py
@@ -0,0 +1,13 @@
+def timeit(func: Callable) -> Callable:
+   return
+
+if __name__ == '__main__':
+	
+    fptr = eopcn(os.envron['path'],'w')
+    n = int(input(),sprit())
+ 
+    function_mapper ={
+     "delay_ma":delay_max,
+     "delay_min": delay_min,
+     "deplay_sum":  delay_sum,
+    }
